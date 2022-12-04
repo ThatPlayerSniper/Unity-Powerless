05bcdd6fdbbd1517ce6bfde903c9b60b3eb065d5
ThatPlayerSniper <114113569+ThatPlayerSniper@users.noreply.github.com>
2022-12-04 22:42:07 +0000

    v

--- My project/Assets/Scripts + ALL/Kits/MedKit_Box.cs
+++ My project/Assets/Scripts + ALL/Kits/MedKit_Box.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 
 public class MedKit_Box : MonoBehaviour
 {
-    [SerializeField] GameObject player;
-    Health health;
-    public int heal = 50;
+    [SerializeField] GameObject player;    //Get's Gameobject to get the "HEALTH" Component from it
+    Health health;                        // HEALTH Script = health variable
+    public int heal = 50;                //Amount of of hp that it will give upon healing
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponent<Health>() != null)
         {
-            health = player.GetComponent<Health>();
-            health.Heal(heal);
+            health = player.GetComponent<Health>(); //Gets component health from player
+            health.Heal(heal);  //gets the variable from the health script and the method "Heal" + the "INT" with the-
+                               //-amount of hp that it will recover
         }
-        Destroy(gameObject);
+        Destroy(gameObject); //Destroys game object upon healing 
     }
 }
